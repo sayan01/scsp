a095cc79d6c788da3fb9ad2a7524bead98067171
Sayan Ghosh <sgic2001@gmail.com>
2022-10-28 02:27:56 +0530

    Modify build files

--- scsp/Program.cs
+++ scsp/Program.cs
@@ -26,8 +26,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseForwardedHeaders();
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    // app.UseHsts();
+    // app.UseHttpsRedirection();
 }
 else{
     app.UseForwardedHeaders();
